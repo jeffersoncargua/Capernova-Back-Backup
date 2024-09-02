0cc5ce38d6b7b1eba28b95085de4296698424adf
Jefferson Cargua <jeffersoncargua@gmail.com>
2024-09-02 13:21:12 -0500

    Proyecto Capernova Back-End se agrego el controlador de ventas

---
+++ CapernovaAPI/Controllers/VentaController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using User.Managment.Data.Data;
+using User.Managment.Data.Models;
+
+namespace CapernovaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        protected ApiResponse _response;
+        public VentaController(ApplicationDbContext db)
+        {
+            _db = db;
+            this._response = new();
+        }
+
+
+        [HttpGet]
+        [Route("getAllVentas")]
+        public async Task<ActionResult<ApiResponse>> GetAllVentas([FromQuery] string? search)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(search))
+                {
+                    var ventasQuery = await _db.VentaTbl.Where(u => u.UserId.Contains(search) || u.LastName.ToLower().Contains(search)).ToListAsync();
+                    _response.isSuccess = true;
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.Message = "Se ha obtenido la lista de las ventas";
+                    _response.Result = ventasQuery;
+                    return Ok(_response);
+                }
+                
+
+                var ventas = await _db.VentaTbl.ToListAsync();
+
+                _response.isSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Message = "Se ha obtenido la lista de las ventas";
+                _response.Result = ventas;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.isSuccess = false;
+                _response.Errors = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+
+        }
+    }
+}
